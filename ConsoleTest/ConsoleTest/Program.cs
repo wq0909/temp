4beb84d80372332230340b57e972282d403b8d47
elppac <elppa.c@gmail.com>
2013-06-24 18:23:30 +0800

    2

--- ConsoleTest/ConsoleTest/Program.cs
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Net;
 using System.Text.RegularExpressions;
@@ -9,47 +11,64 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            CookieContainer cookie = new CookieContainer();
-
-
-            string output = TestUrl.sendUrl("https://login.alibaba.com/", ref cookie);
-
-            output = TestUrl.sendUrl("https://login.alibaba.com/xman/xlogin.js?pd=alibaba&pageFrom=standardlogin&u_token=&xloginPassport=aliqatest01&xloginPassword=1227qa01aetest&dmtrack_pageid=79001dc26e4bd84051c7e1cf13f74ca14e713b5b05", ref cookie);
-
-            var match = Regex.Match(output, ":\"(.*?)\"}$");
-            string tokenString = match.Groups[1].Value;
-
-            output = TestUrl.sendUrl("https://passport.alipay.com/mini_apply_st.js?site=4&callback=window.xmanDealTokenCallback&token=" + tokenString, ref cookie);
-
-            output = TestUrl.sendUrl("https://login.alibaba.com/validateST.htm?st=" + tokenString + "&pd=alibaba&pageFrom=standardlogin&u_token=&xloginPassport=aliqatest01&xloginPassword=1227qa01aetest&dmtrack_pageid=79001dc26e4bd84051c7e1cf13f74ca14e713b5b05", ref cookie);
-
-            output = TestUrl.sendUrl("http://sh.vip.alibaba.com/index.htm", ref cookie);
-
-            Console.WriteLine(output.IndexOf("CRM_notice.html") > -1 ? "-----------------------------ok-------------------------------" : "fxxx");
-            Console.ReadLine();
-
+            TestUrl.action();
         }
     }
     public static class TestUrl
     {
-        public static string sendUrl(string url, ref CookieContainer cookie)
+        public static void action()
+        {
+            CookieContainer cookie = new CookieContainer();
+            NameValueCollection headderColl = new NameValueCollection();
+            headderColl.Add("Referer", "https://login.alibaba.com");
+
+             string output = TestUrl.sendUrl("https://login.alibaba.com/", ref cookie, headderColl);
+
+            output = TestUrl.sendUrl("https://login.alibaba.com/xman/xlogin.js?pd=alibaba&pageFrom=standardlogin&u_token=&xloginPassport=aliqatest01&xloginPassword=1227qa01aetest&dmtrack_pageid=79001dc26e4bd84051c7e1cf13f74ca14e713b5b05", ref cookie, headderColl);
+
+            var match = Regex.Match(output, ":\"(.*?)\"}$");
+            string tokenString = match.Groups[1].Value;
+
+            headderColl.Add("Referer", "https://login.alibaba.com");
+            output = TestUrl.sendUrl("https://passport.alipay.com/mini_apply_st.js?site=4&callback=window.xmanDealTokenCallback&token=" + tokenString, ref cookie, headderColl);
+
+            headderColl.Add("Referer", "https://login.alibaba.com");
+            output = TestUrl.sendUrl("https://login.alibaba.com/validateST.htm?st=" + tokenString + "&pd=alibaba&pageFrom=standardlogin&u_token=&xloginPassport=aliqatest01&xloginPassword=1227qa01aetest&dmtrack_pageid=79001dc26e4bd84051c7e1cf13f74ca14e713b5b05", ref cookie, 
+                headderColl);
+
+            Console.WriteLine(output);
+
+            output = TestUrl.sendUrl("http://sh.vip.alibaba.com/index.htm", ref cookie, headderColl);
+
+            Console.WriteLine(output.IndexOf("CRM_notice.html") > -1 ? "-----------------------------ok-------------------------------" : "fxxx");
+            Console.ReadLine();
+        }
+        public static string sendUrl(string url, ref CookieContainer cookie, NameValueCollection headerColl)
         {
             // Create a request for the URL. 		
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
 
-            //实例化CookieContainer对象
+            /*//实例化CookieContainer对象
             System.Net.CookieContainer cookieContainer = new System.Net.CookieContainer();
             //设置附加的Cookie
             cookieContainer.SetCookies(new Uri(url), "");
-
+            */
 
             // If required by the server, set the credentials.
             request.Credentials = CredentialCache.DefaultCredentials;
             request.Method = "GET";
             request.UserAgent = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:21.0)Gecko/20100101 Firefox/21.0";
             request.KeepAlive = true;
-            request.CookieContainer = cookieContainer;
+            request.Accept = "*/*";
+            request.CookieContainer = cookie;
+            if (headerColl != null)
+            {
+                if( !string.IsNullOrEmpty(headerColl.Get("Referer"))){
+                    request.Referer = headerColl.Get("Referer");
+                }
+            }
 
+            Console.WriteLine("##");
             Console.WriteLine(url);
 
             /*System.Net.CookieContainer cs = new System.Net.CookieContainer();
@@ -70,6 +89,13 @@ namespace ConsoleTest
             StreamReader reader = new StreamReader(dataStream);
             // Read the content.
             string responseFromServer = reader.ReadToEnd();
+
+            foreach (Cookie c in response.Cookies)
+            {
+                Console.WriteLine(c.Name + ":" + c.Value);
+                cookie.Add(c);
+            }
+            
             // Display the content.
             // Console.WriteLine(responseFromServer);
             // Cleanup the streams and the response.
